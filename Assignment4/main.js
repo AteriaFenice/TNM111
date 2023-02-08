f33428233556cf04b5c3124148272273fbf6f8dc
Malva <halva.malva@gmail.com>
2023-02-08 12:41:17 +0100

    names!!!

--- Assignment4/main.js
+++ Assignment4/main.js
@@ -9,15 +9,15 @@ fetch(url)
 
         let nodes = data.nodes
         let links = data.links
-        //let colour = data.colour
-        //console.log(colour)
         
+        // Node link diagram
         var simulation = d3.forceSimulation(nodes)
             .force('charge', d3.forceManyBody().strength(-70))
             .force('center', d3.forceCenter(width / 2, height / 2))
             .force('link', d3.forceLink().links(links))
             .on('tick', ticked);
         
+        // Links
         function updateLinks() {
             var u = d3.select('.links')
                 .selectAll('line')
@@ -37,32 +37,46 @@ fetch(url)
                 });
         }
         
+        // Nodes
         function updateNodes() {
             var u = d3.select('.nodes')
-                .selectAll('text')
+                .selectAll('circle')
                 .data(nodes)
-                .join('text')
-                .text(function(d){
-                    return d.name
-                })
+                .join('circle')
+                .attr('r', 5)
                 .style('fill', function(d) {
                     return d.colour;
                 })
-                .attr('x', function(d) {
+                .attr('cx', function(d) {
                     return d.x
                 })
-                .attr('y', function(d) {
+                .attr('cy', function(d) {
                     return d.y
                 })
-                .attr('dy', function(d) {
-                    return 5
+                .on('mouseover', function(d,i){
+                    d3.select(".info .name").text(d['name']);
+                    d3.select(".info").style('visibility', 'visible');
+                    d3.select(this)
+                        .transition()
+                        .duration(50)
+                        .attr('r', 10);
+                })
+                .on('mouseout', function(d,i){
+                    d3.select(".info").style('visibility', 'hidden');
+                    d3.select(this)
+                        .transition()
+                        .duration(50)
+                        .attr('r', 5);
                 });
+
         }
         
         function ticked() {
             updateLinks()
             updateNodes()
         }
+
+
     })
 
 
