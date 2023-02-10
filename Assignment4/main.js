1822baac2f0dc07f50453e3a62599f0821c8f9ce
Maria <maria.brunned@gmail.com>
2023-02-10 12:40:52 +0100

    small fixes from the merge

--- Assignment4/main.js
+++ Assignment4/main.js
@@ -12,17 +12,17 @@ fetch(url)
         //console.log(data_clone);
         console.log(data2)
 
-        let nodes = data.nodes
-        let links = data.links
+        let nodes1 = data.nodes
+        let links1 = data.links
 
         let nodes2 = data2.nodes
         let links2 = data2.links
         
         // Node link diagram
-        var simulation = d3.forceSimulation(nodes)
+        var simulation = d3.forceSimulation(nodes1)
             .force('charge', d3.forceManyBody().strength(-50))
             .force('center', d3.forceCenter(width / 2, height / 2))
-            .force('link', d3.forceLink().links(links))
+            .force('link', d3.forceLink().links(links1))
             .on('tick', ticked);
 
         // Second node link diagram
@@ -36,7 +36,7 @@ fetch(url)
         function updateLinks() {
             var u = d3.select('.links1')
                 .selectAll('line')
-                .data(links)
+                .data(links1)
                 .join('line')
                 .attr('x1', function(d) {
                     return d.source.x
@@ -49,6 +49,23 @@ fetch(url)
                 })
                 .attr('y2', function(d) {
                     return d.target.y
+                })
+                .style('stroke', '#ededed', )
+                .style('stroke-width', '1px')
+                .on('mouseover', function(d){
+                    d3.select(".infoLink .source").text(d['source'].name);
+                    d3.select(".infoLink .target").text(d['target'].name);
+                    d3.select(".infoLink .value").text(d['value']);
+                    d3.select(".infoLink").style('visibility', 'visible');
+                    d3.select(this)
+                        .style('stroke', '#d3d3d3', )
+                        .style('stroke-width', '3px');
+                })
+                .on('mouseout', function(){
+                    d3.select(".infoLink").style('visibility', 'hidden');
+                    d3.select(this)
+                    .style('stroke', '#ededed', )
+                    .style('stroke-width', '1px');
                 });
         }
 
@@ -91,8 +108,8 @@ fetch(url)
         // Nodes
         function updateNodes() {
             var u = d3.select('.nodes1')
-                .selectAll('text')
-                .data(nodes)
+                .selectAll('circle')
+                .data(nodes1)
                 .join('circle')
                 .attr('r', 5)
                 .style('fill', function(d) {
@@ -103,6 +120,9 @@ fetch(url)
                 })
                 .attr('cy', function(d) {
                     return d.y
+                })
+                .attr('dy', function(d){
+                    return 5
                 })
                 .on('mouseover', function(d){
                     d3.select(".info .name").text(d['name']);
@@ -125,24 +145,35 @@ fetch(url)
 
         function updateNodes2() {
             var u = d3.select('.nodes2')
-                .selectAll('text')
-                .data(nodes2)
-                .join('text')
-                .text(function(d){
-                    return d.name
-                })
-                .style('fill', function(d) {
-                    return d.colour;
-                })
-                .attr('x', function(d) {
-                    return d.x
-                })
-                .attr('y', function(d) {
-                    return d.y
-                })
-                .attr('dy', function(d) {
-                    return 5
-                });
+            .selectAll('circle')
+            .data(nodes2)
+            .join('circle')
+            .attr('r', 5)
+            .style('fill', function(d) {
+                return d.colour;
+            })
+            .attr('cx', function(d) {
+                return d.x
+            })
+            .attr('cy', function(d) {
+                return d.y
+            })
+            .on('mouseover', function(d){
+                d3.select(".info .name").text(d['name']);
+                d3.select(".info .value").text(d['value']);
+                d3.select(".info").style('visibility', 'visible');
+                d3.select(this)
+                    .transition()
+                    .duration(50)
+                    .attr('r', 10);
+            })
+            .on('mouseout', function(){
+                d3.select(".info").style('visibility', 'hidden');
+                d3.select(this)
+                    .transition()
+                    .duration(50)
+                    .attr('r', 5);
+            });
         }
         
         function ticked() {
