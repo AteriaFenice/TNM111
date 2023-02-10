7cdef416b6d05b2c4831905c3288e6d5cc008c7a
Maria <maria.brunned@gmail.com>
2023-02-10 09:33:43 +0100

    Fixed some changes

--- Assignment4/main.js
+++ Assignment4/main.js
@@ -5,19 +5,30 @@ fetch(url)
     .then(res => res.json())
     //.then(data => console.log(data))
     .then(data => {
-        var width = 1000, height = 800
+        var width = 600, height = 800
 
         let nodes = data.nodes
         let links = data.links
-        //let colour = data.colour
-        //console.log(colour)
+
+        let nodes2 = data.nodes
+        let links2 = data.links
         
-        var simulation = d3.forceSimulation(nodes)
-            .force('charge', d3.forceManyBody().strength(-70))
+        // Node link diagram
+        var simulation = d3
+            .forceSimulation(nodes)
+            .force('charge', d3.forceManyBody().strength(-50))
+            .force('center', d3.forceCenter(width / 4, height / 2))
+            .force('link', d3.forceLink().links(links))
+            .on('tick', ticked);
+
+        // Second node link diagram
+        var simulation2 = d3.forceSimulation(nodes)
+            .force('charge', d3.forceManyBody().strength(-50))
             .force('center', d3.forceCenter(width / 2, height / 2))
             .force('link', d3.forceLink().links(links))
             .on('tick', ticked);
-        
+
+        // Links
         function updateLinks() {
             var u = d3.select('.links')
                 .selectAll('line')
