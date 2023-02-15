7bcdf6f135d1d9685730f4ae0a7592ba988012bc
Malva <halva.malva@gmail.com>
2023-02-15 14:27:21 +0100

    Slider + functionality

--- Assignment4/main.js
+++ Assignment4/main.js
@@ -49,7 +49,7 @@ fetch(url)
                 .on('mouseout', function(){
                     d3.select(".infoLink").style('visibility', 'hidden');
                     d3.select(this)
-                    .style('stroke', '#ededed', )
+                    .style('stroke', '#ededed')
                     .style('stroke-width', '1px');
                 });
         }
@@ -84,11 +84,197 @@ fetch(url)
                     d3.select(this)
                         .transition()
                         .duration(50)
+                        .style('stroke', '#ededed')
                         .attr('r', 5);
                 });
+        }
+
+        // Layout for slider
+        layout = ({
+            width: 400,
+            height: 300,
+            margin: {
+              top: 130,
+              bottom: 135,
+              left: 40,
+              right: 40
+            }
+        })
+        
+        // Slider function, example from: https://observablehq.com/@sarah37/snapping-range-slider-with-d3-brush
+        slider = function(min, max, starting_min=min, starting_max=max) {
+            var range = [min, max + 1]
+            var starting_range = [starting_min, starting_max + 1]
+
+            // set width and height of svg
+            var w = layout.width
+            var h = layout.height
+            var margin = layout.margin
+
+            // Dimensions of slider bar
+            var width = w - margin.left - margin.right;
+            var height = h - margin.top - margin.bottom;
+
+            // create x scale
+            var x = d3.scaleLinear()
+                .domain(range) // data space
+                .range([0, width]); // display space
+
+            // create svg and translated g
+            var svg = d3.select('svg')//d3.select(DOM.svg(w, h))
+            const g = svg.append('g')
+                            .attr('transform',`translate(${margin.left}, ${margin.top})`)
+
+
+            // draw background lines
+            g.append('g').selectAll('line')
+                .data(d3.range(range[0], range[1]+1))
+                .enter()
+                .append('line')
+                .attr('x1', d => x(d))
+                .attr('x2', d => x(d))
+                .attr('y1', 0)
+                .attr('y2', height)
+                .style('stroke', '#ccc')
+
+            // labels
+            var labelL = g.append('text')
+                .attr('id', 'labelleft')
+                .attr('x', 0)
+                .attr('y', height + 5)
+                .text(range[0])
+
+            var labelR = g.append('text')
+                .attr('id', 'labelright')
+                .attr('x', 0)
+                .attr('y', height + 5)
+                .text(range[1])
+
+            // define brush
+            var brush = d3.brushX()
+                .extent([[0, 0], [width, height]])
+                .on('brush', function(){
+                    var s = d3.event.selection;
+
+                    // update and move labels
+                    labelL.attr('x', s[0])
+                        .text(Math.round(x.invert(s[0])))
+                    labelR.attr('x', s[1])
+                        .text(Math.round(x.invert(s[1])) - 1)
+
+                    // move brush handles
+                    handle.attr('display', null).attr('transform', function(d, i) {
+                        return 'translate(' + [ s[i], -height / 4] + ')';
+                    });
+
+                    // update view
+                    svg.node().value = s.map(d => Math.round(x.invert(d)));
+                    //svg.node().dispatchEvent(new CustomEvent('input'));
+                    // event for range sliders
+                    let event = new Event('change');
+                    eventhandler.dispatchEvent(event);
+                })
+                .on('end', function() {
+                    if(!d3.event.sourceEvent) return;
+                    var d0 = d3.event.selection.map(x.invert);
+                    var d1 = d0.map(Math.round)
+                    d3.select(this).transition().call(d3.event.target.move, d1.map(x))
+                })
+
+            // append brush to g
+            var gBrush = g.append('g')
+                .attr('class', 'brush')
+                .call(brush)
+
+            // add brush to handles
+            var brushResizePath = function(d) {
+                var e = +(d.type == 'e'),
+                    x = e ? 1 : -1,
+                    y = height / 2;
+                return 'M' + (.5 * x) + "," + y + "A6,6 0 0 " + e + " " + (6.5 * x) + "," + (y + 6) + "V" + (2 * y - 6) +
+                "A6,6 0 0 " + e + " " + (.5 * x) + "," + (2 * y) + "Z" + "M" + (2.5 * x) + "," + (y + 8) + "V" + (2 * y - 8) +
+                "M" + (4.5 * x) + "," + (y + 8) + "V" + (2 * y - 8);
+            }
+
+            var handle = gBrush.selectAll(".handle--custom")
+                .data([{type: "w"}, {type: "e"}])
+                .enter()
+                .append("path")
+                .attr("class", "handle--custom")
+                .attr("stroke", "#000")
+                .attr("fill", '#eee')
+                .attr("cursor", "ew-resize")
+                .attr("d", brushResizePath);
+
+            // clicking outside selected area will select small peice there
+            gBrush.selectAll('overlay')
+                .each(function(d){
+                    d.type = 'selection';
+                })
+                .on('mousedown touchstart', brushcentered)
+
+            function brushcentered(){
+                var dx = x(1) - x(0),
+                    cx = dd3.mouse(this)[0],
+                    x0 = cx - dx / 2,
+                    x1 = cx + dx / 2;
+
+                d3.select(this.parentNode).call(brush.move, x1 > width ? [width - dx, width] : x0 < 0 ? [0, dx] : [x0, x1]);
+            }
+
+            // select entire starting range
+            gBrush.call(brush.move, starting_range.map(x))
+
+            // get values
+            var getRange = function(){
+                var range = d3.brushSelection(gBrush.node()).map(d => Math.round(x.invert(d)))
+                return range
+            }
+
+            return svg.node, {getRange : getRange} // myslider.getRange returns selected range
 
         }
-        
+
+        // Set range values and call slider
+        const rangeMax = Math.max.apply(Math, links.map(function(d){return d.value}))
+        const rangeMin = Math.min.apply(Math, links.map(function(d){return d.value}))
+        myslider = slider(rangeMin, rangeMax, undefined, undefined)
+
+        // Update node link diagram based on slider
+        d3.select('#eventhandler')
+        .on('change', function(){
+            // change visibility of links and nodes)
+            links = data.links
+            nodes = data.nodes
+            let filteredLinks = links
+                .filter(function (d) { 
+                    linksFiltered = d.value <= myslider.getRange()[1] && d.value >= myslider.getRange()[0]; 
+                    return linksFiltered
+                });
+           // console.log(filteredLinks)
+            links = filteredLinks
+            updateLinks()
+            
+            // sources and targets of remaining links
+            linkSource = links.map(function(d){return d.source.name})
+            linkTarget = links.map(function(d){return d.target.name})
+            //console.log(linkSource, linkTarget)
+
+            let filteredNodes = nodes
+                .filter(function (d) { 
+                    console.log(d.name)
+                    // om node finns som source eller target hos links ska den va kvar
+                    nodesFiltered = linkSource.includes(d.name) || linkTarget.includes(d.name)
+                    return nodesFiltered
+                })
+            console.log(filteredNodes)
+            nodes = filteredNodes
+            updateNodes()
+
+
+        });
+
+
         function ticked() {
             updateLinks()
             updateNodes()
